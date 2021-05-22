88b6c44f7362df0f8e12ca4710dee1e040ea5e35
sergey_sedov <sergey.sedov.ss@yandex.ru>
2021-05-22 18:07:52 +0300

    Initial Commit

---
+++ sNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sNotes
+//
+//  Created by Apple on 22.05.2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
